baac53d3ac59e0bf61ae432660a919a7b4a0a7a1
teste <141754027+luabraga0211@users.noreply.github.com>
2024-11-24 19:36:58 -0300

    at
    at

--- src/pages/Iniciologado/Iniciologado.jsx
+++ src/pages/Iniciologado/Iniciologado.jsx
@@ -1,38 +1,38 @@
 import './Iniciologado.css';
 import logo from './logo.png'
 import { Link } from 'react-router-dom'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 
 const Iniciologado = () => {
     return (
         <div>
-         <div className="navbar">
-            <button>
-             Fazer Novo Cadastro
-            </button>
-            <button>
-            Ver Cadastros
-            </button>
-            <button>
-            Controle de Entrada e Saída
-            </button>
-          </div>
+            <div className="navbar">
+                <button>
+                    Fazer Novo Cadastro
+                </button>
+                <button>
+                    Ver Cadastros
+                </button>
+                <button>
+                    Controle de Entrada e Saída
+                </button>
+            </div>
+            <div className="icons">
+                <FontAwesomeIcon icon={faUser} className="icon" />
+                <FontAwesomeIcon icon={faSignOutAlt} className="icon" />
+            </div>
             <div className="conteudo">
-                    <div className="text">
-                        <p>Bem-vindo ao nosso site! Somos uma empresa especializada em controle de entrada e saída para empresas de tecnologia. Nosso objetivo é fornecer soluções eficientes e seguras para gerenciar o acesso de funcionários, visitantes e prestadores de serviços em sua empresa.</p>
-                    </div>
-                    <div className="logo">
-                        <Link to="/">
-                            <img src={logo} alt="Logo da AccessTrack Solutions" />
-                        </Link>
-                        <p>AccessTrack Solutions</p>
-                    </div>
+                <div className="text">
+                    <p>Bem-vindo ao nosso site! Somos uma empresa especializada em controle de entrada e saída para empresas de tecnologia. Nosso objetivo é fornecer soluções eficientes e seguras para gerenciar o acesso de funcionários, visitantes e prestadores de serviços em sua empresa.</p>
                 </div>
-                <div class="icons">
-                <i class="fas fa-user-circle">
-                </i>
-                <i class="fas fa-sign-out-alt">
-                </i>
+                <div className="logo">
+                    <Link to="/">
+                        <img src={logo} alt="Logo da AccessTrack Solutions" />
+                    </Link>
+                    <p>AccessTrack Solutions</p>
+                </div>
             </div>
         </div>
     );
